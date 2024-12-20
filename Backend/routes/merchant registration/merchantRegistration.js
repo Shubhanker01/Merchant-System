e5d0e41a259fdaa0b4883f291a90a35d1b1c143f
Shubhanker01 <shubhanker40@gmail.com>
2024-12-20 16:05:46 +0530

    send email module added

--- Backend/routes/merchant registration/merchantRegistration.js
+++ Backend/routes/merchant registration/merchantRegistration.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { Merchant } = require('../../models/merchant.model')
+const sendEmail = require('../../utils/sendEmail')
 
 router.post('/registration', async (req, res) => {
     try {
@@ -11,9 +12,14 @@ router.post('/registration', async (req, res) => {
             password: req.body.password,
             isVerified: false,
         })
+        let response = await sendEmail(req.body.email)
+        console.log(response)
         res.json({ message: "Merchant registered successfully" })
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
+        if (error.code === 11000) {
+            res.status(401).send("This email is already registered")
+        }
     }
 })
 
---
+++ Backend/utils/sendEmail.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const generatePassword = require('./generatePassword')
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    service:'gmail',
+    auth: {
+        email: process.env.EMAIL,
+        password: process.env.PASSWORD
+    }
+})
+
+const sendEmail = async (recepient) => {
+    try {
+        let temporaryPassword = generatePassword()
+        const sendPassword = await transporter.sendMail({
+            from: process.env.EMAIL,
+            to: recepient,
+            subject: "Enter the below password to verify your account. This password is valid for 5 minutes",
+            text: temporaryPassword
+        })
+        return { password: temporaryPassword, id: sendPassword.messageId }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+module.exports = sendEmail
