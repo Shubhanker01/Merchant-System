1932bb6eae05fe1dc2e233e66f4b8cf4626f80f6
Shubhanker01 <shubhanker40@gmail.com>
2024-12-27 13:36:55 +0530

    created controller

---
+++ Backend/controllers/merchant login controller/merchantlogin.controller.js
@@ -0,0 +1,32 @@
+const { Merchant } = require('../../models/merchant.model')
+const bcrypt = require('bcrypt')
+const { generateToken } = require('../../utils/generateToken')
+
+const merchantLogin = async (req, res) => {
+    try {
+        let email = req.body.email
+        let password = req.body.password
+        if (!email || !password) {
+            return res.status(401).send("Please fully enter your details")
+        }
+        else {
+            let user = await Merchant.findOne({ email: email })
+            if (!user) {
+                return res.status(404).send("User not found")
+            }
+            let compare = await bcrypt.compare(password, user.password)
+            if (!compare) {
+                return res.status(401).send("Incorrect Password")
+            }
+            else {
+                let token = generateToken(req.body.email)
+                return res.status(200).cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', }).send("Successfully logged in")
+            }
+        }
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+
+}
+module.exports = merchantLogin
--- Backend/routes/merchant login/merchantLogin.js
+++ Backend/routes/merchant login/merchantLogin.js
@@ -1,35 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { Merchant } = require('../../models/merchant.model')
-const bcrypt = require('bcrypt')
-const { generateToken } = require('../../utils/generateToken')
-const { limiter } = require('../../utils/rateLimiter')
 
-router.post('/login', limiter, async (req, res) => {
-    try {
-        let email = req.body.email
-        let password = req.body.password
-        if (!email || !password) {
-            return res.status(401).send("Please fully enter your details")
-        }
-        else {
-            let user = await Merchant.findOne({ email: email })
-            if (!user) {
-                return res.status(404).send("User not found")
-            }
-            let compare = await bcrypt.compare(password, user.password)
-            if (!compare) {
-                return res.status(401).send("Incorrect Password")
-            }
-            else {
-                let token = generateToken(req.body.email)
-                return res.status(200).cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', }).send("Successfully logged in")
-            }
+const { limiter } = require('../../middlewares/rateLimiter')
 
-        }
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+const merchantLogin = require('../../controllers/merchant login controller/merchantlogin.controller')
 
+
+router.route('/login').post(limiter, merchantLogin)
 module.exports = router
