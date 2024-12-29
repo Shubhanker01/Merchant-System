c3d13185df744b48d762119e52620f953e86460d
Shubhanker01 <shubhanker40@gmail.com>
2024-12-29 15:59:23 +0530

    Login and Signup Components

--- Frontend/merchant-client/src/Components/Login.jsx
+++ Frontend/merchant-client/src/Components/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>I am a login page</div>
+    <>
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 m-[0px_auto]">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Login to your account
+          </h1>
+          <form className="space-y-4 md:space-y-6" action="#">
+
+            <div>
+              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+            </div>
+            <div>
+              <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+              <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+            </div>
+            <button type="submit" className="w-full text-gray-800 bg-gray-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              Forgot your password? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Click here</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+
   )
 }
 
