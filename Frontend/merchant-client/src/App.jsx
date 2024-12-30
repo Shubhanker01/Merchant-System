7c6acd530574b243dacf27dbf6b2357e5b8c0a26
Shubhanker01 <shubhanker40@gmail.com>
2024-12-30 09:37:40 +0530

    changes folder structure

--- Frontend/merchant-client/src/App.jsx
+++ Frontend/merchant-client/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import Home from './Components/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Components/Login'
-import Signup from './Components/Signup'
+import Signup from './Components/User Registration/Signup'
+import EmailVerification from './Components/User Registration/EmailVerification'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/email-verification' element={<EmailVerification />}></Route>
         </Routes>
       </BrowserRouter>
 
---
+++ Frontend/merchant-client/src/Components/User Registration/PasswordSet.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function PasswordSet() {
+    return (
+        <div>
+            <h1 className='text-center text-2xl mt-[30px] mb-[15px]'>This is last step. Please set your password to complete your registration</h1>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 m-[0px_auto]">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Set your password
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
+                        </div>
+                        <div>
+                            <label for="conf-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                            <input type="password" name="conf-password" id="conf-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
+                        </div>
+
+                        <button type="submit" className="w-full text-gray-800 bg-gray-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Continue</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default PasswordSet
