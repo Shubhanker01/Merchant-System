7fc1776ca910446f33380ae72fd03956985c138f
Shubhanker01 <shubhanker40@gmail.com>
2024-12-31 12:35:13 +0530

    changes in signup

--- Frontend/merchant-client/src/Components/User Registration/Signup.jsx
+++ Frontend/merchant-client/src/Components/User Registration/Signup.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { setMerchant } from '../../Features/Merchant/merchantSlice'
 import { toast } from 'react-toastify'
 function Signup() {
-
+    let [username, setUsername] = useState("")
+    let [email, setEmail] = useState("")
     let merchant = useSelector(state => state.merchant)
     let dispatch = useDispatch()
     const addMerchant = (e) => {
         e.preventDefault()
-        dispatch(setMerchant({ username: e.target.username.value, email: e.target.email.value }))
-        toast("successfully submitted", { position: 'top-center',className:'bg-sky-950 text-slate-100' })
+        dispatch(setMerchant({ username: username, email: email }))
+        toast("successfully submitted", { position: 'top-center', className: 'bg-sky-950 text-slate-100' })
+        setUsername("")
+        setEmail("")
     }
 
     return (
@@ -27,11 +30,11 @@ function Signup() {
                     <form class="space-y-4 md:space-y-6" onSubmit={addMerchant}>
                         <div>
                             <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
-                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required={true} />
+                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required={true} value={username} onChange={(e) => { setUsername(e.target.value) }} />
                         </div>
                         <div>
                             <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required={true} />
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required={true} value={email} onChange={(e) => { setEmail(e.target.value) }} />
                         </div>
 
                         <button type="submit" class="w-full text-gray-800 bg-gray-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Verify account</button>
