e922e0808eec62b0eb9d65efa956e0a583715097
Shubhanker01 <shubhanker40@gmail.com>
2025-01-13 15:00:29 +0530

    main app design in progress

--- Frontend/merchant-client/src/Components/Header/NavbarApp.jsx
+++ Frontend/merchant-client/src/Components/Header/NavbarApp.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NavbarApp() {
+    const [toggle, setToggle] = useState(false)
+
+    const handleToggle = () => {
+        if (toggle == false) {
+            setToggle(true)
+            document.querySelector('#navBtn').classList.add('close')
+        }
+        else {
+            setToggle(false)
+            document.querySelector('#navBtn').classList.remove('close')
+        }
+    }
     return (
         <div>
             <div className='fixed w-full'>
                 {/* Navbar */}
                 <nav className="bg-slate-900 p-4">
-                    <div className="container mx-auto flex flex-col  justify-between items-center">
+                    <div className="container mx-auto flex sm:flex-col  justify-between items-center">
                         <div className='flex items-center'>
                             <img width="48" height="48" src="https://img.icons8.com/color/48/bid.png" alt="bid" />
                             <h1 className="text-white font-bold sm:text-2xl mb-4 lg:mb-0 hover:text-orange-600 hover:cursor-pointer text-base">Merchant Management System </h1>
@@ -14,7 +26,7 @@ function NavbarApp() {
 
                         {/* Hamburger menu for small screens */}
                         <div className="lg:hidden">
-                            <button className="text-white focus:outline-none" id='navBtn'>
+                            <button className="text-white focus:outline-none" id='navBtn' onClick={handleToggle}>
                                 <div className='btn-line'></div>
                                 <div className='btn-line'></div>
                                 <div className='btn-line'></div>
@@ -22,13 +34,20 @@ function NavbarApp() {
                         </div>
 
                         {/* Navigation links */}
-                        <div className={`lg:flex flex-col lg:flex-row lg:space-x-4 lg:mt-0 mt-4 flex flex-col items-center text-xl`}>
-                            <a href="/" className="text-white text-base px-4 py-2 hover:text-orange-600 ">Home</a>
-                            <a href="#Projects" className="text-white text-base  px-4 py-2  hover:text-orange-600">Message</a>
-                            <a href="/" className="text-white text-base  px-4 py-2  hover:text-orange-600">About</a>
-                            <a href="/" className="text-white text-base  px-4 py-2  hover:text-orange-600">Contact Me</a>
-                            <button className='text-slate-100 bg-orange-600 rounded-md text-base p-2'>Logout</button>
-                        </div>
+                        {
+                            toggle == false ?
+                                <div className='lg:flex flex-col lg:flex-row lg:space-x-4 lg:mt-0 mt-4 flex flex-col items-center text-xl'>
+                                    <a href="/" className="text-white text-base px-4 py-2 hover:text-orange-600 ">Home</a>
+                                    <a href="#Projects" className="text-white text-base  px-4 py-2  hover:text-orange-600">Message</a>
+                                    <a href="/" className="text-white text-base  px-4 py-2  hover:text-orange-600">About</a>
+                                    <a href="/" className="text-white text-base  px-4 py-2  hover:text-orange-600">Contact Me</a>
+                                    <button className='text-slate-100 bg-orange-600 rounded-md text-base p-2'>Logout</button>
+                                </div>:
+                                <div>
+                                    
+                                </div>
+                        }
+
                     </div>
 
                 </nav>
