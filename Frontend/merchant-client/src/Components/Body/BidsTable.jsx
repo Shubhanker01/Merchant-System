c54897d28b8eb0dd0d7edf76e2fbc4079d920658
Shubhanker01 <shubhanker40@gmail.com>
2025-01-16 09:37:22 +0530

    Bids Table

--- Frontend/merchant-client/src/Components/Body/BidsTable.jsx
+++ Frontend/merchant-client/src/Components/Body/BidsTable.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 
 function BidsTable() {
     return (
-        <div className='mt-[100px]'>
-            <table className='table-auto'>
-                <thead>
-                    <tr>
-                        <th>Sr No</th>
-                        <th>Bidder Name</th>
-                        <th>Title</th>
-                        <th>Price</th>
-                        <th>Opening Date</th>
-                        <th>Closing Date</th>
+        <div className='m-[100px_auto]'>
+            <table className='table-auto text-base text-gray-200 bg-gray-900 rounded-md ml-auto mr-auto'>
+                <thead className=''>
+                    <tr className=''>
+                        <th className='px-4 py-4'>Sr No</th>
+                        <th className='px-4'>Bidder Name</th>
+                        <th className='px-24'>Title</th>
+                        <th className='px-8'>Price</th>
+                        <th className='px-4'>Opening Date</th>
+                        <th className='px-4'>Closing Date</th>
                     </tr>
                 </thead>
+                <tbody className='bg-gray-300 text-gray-800 rounded-b-md'>
+                    <tr>
+                        <td className='px-4 py-4'>1.</td>
+                        <td className='px-4'>Bidder 1</td>
+                        <td className='px-24'>Auction on Building</td>
+                        <td className='px-8'>Rs 101</td>
+                        <td className='px-4'>10-11-2025</td>
+                        <td className='px-4'>10-11-2026</td>
+                    </tr>
+                </tbody>
+                <tbody className='bg-gray-300 text-gray-800 rounded-b-md'>
+                    <tr>
+                        <td className='px-4 py-4'>1.</td>
+                        <td className='px-4'>Bidder 1</td>
+                        <td className='px-24'>Auction on Building</td>
+                        <td className='px-8'>Rs 101</td>
+                        <td className='px-4'>10-11-2025</td>
+                        <td className='px-4'>10-11-2026</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
