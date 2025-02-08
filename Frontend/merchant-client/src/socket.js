3876d17bf02a7cb0a1e46f3c7a67ca9f57160778
Shubhanker01 <shubhanker40@gmail.com>
2025-02-08 12:26:00 +0530

    change socket url

--- Frontend/merchant-client/src/socket.js
+++ Frontend/merchant-client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:8000'
+const URL = `${import.meta.env.VITE_PROD_SERVER}`
 
 export const socket = io(URL, {
     autoConnect: false
