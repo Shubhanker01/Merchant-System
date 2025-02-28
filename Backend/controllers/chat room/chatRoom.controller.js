aca31dd2268c44a8dcd33d7721a1d3f5ee5da9d0
Shubhanker01 <shubhanker40@gmail.com>
2025-02-28 12:49:13 +0530

    Add participant controller+route added

--- Backend/controllers/chat room/chatRoom.controller.js
+++ Backend/controllers/chat room/chatRoom.controller.js
@@ -1,23 +1,45 @@
 const { groupChat } = require('../../models/groupchat.mode')
 const { Merchant } = require('../../models/merchant.model')
+const mongoose = require('mongoose')
 // create a new group chat
 const createGroupChat = async (req, res) => {
     try {
         let email = req.body.email
         let name = req.body.name
         let admin = await Merchant.findOne({ email: email })
+        if (!admin) {
+            return res.status(400).send("Some error occured")
+        }
         await groupChat.create({
             name: name,
-            admin: admin._id
+            admin: admin._id,
+            participants: admin._id
         })
-        res.send("Group successfully created")
+        return res.send("Group successfully created")
     } catch (error) {
         console.log(error)
     }
 }
 // add participants
+const addParticipants = async (req, res) => {
+    try {
+        // only admin can add participants
+        let participant = req.body.participants
+        let id = req.body.id
+        let chat = await groupChat.findOne({ _id: id })
+        if (!chat) {
+            return res.status(400).send("Error Chat not found")
+        }
+        chat.participants.push(...participant)
+        await chat.save()
+        return res.send("Successfully added participants")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 // delete chat group
 
 
-module.exports = {createGroupChat}
+module.exports = { createGroupChat, addParticipants }
 
--- Backend/routes/chat room/chatRoom.routes.js
+++ Backend/routes/chat room/chatRoom.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {createGroupChat} = require('../../controllers/chat room/chatRoom.controller')
+const { createGroupChat, addParticipants } = require('../../controllers/chat room/chatRoom.controller')
 // route for creating group chat
 router.route('/createchat').post(createGroupChat)
-
+// route for adding members
+router.route('/addmembers').post(addParticipants)
 module.exports = router
