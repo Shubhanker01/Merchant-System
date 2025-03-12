a0d99a5de33aaf90b75a93f19b6d6d0d7ca10786
Shubhanker01 <shubhanker40@gmail.com>
2025-03-12 12:00:48 +0530

    Add AddParticipant modal

--- Frontend/merchant-client/src/Components/Main App/AddParticipant.jsx
+++ Frontend/merchant-client/src/Components/Main App/AddParticipant.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import checkAdmin from '../../utils/checkAdmin'
+import AddParticipantModal from '../Popup/AddParticipantModal'
 
 function AddParticipant({ participants, admin }) {
     let isAdmin = checkAdmin(participants, admin)
+    let [modal, setModal] = useState(false)
     return (
         <>
             <div className={`${isAdmin ? 'block w-[80%] m-4' : 'hidden'}`}>
-                <button className='bg-blue-600 text-slate-100 rounded-md p-2'>
+                <button onClick={() => setModal(true)} className='bg-blue-600 text-slate-100 rounded-md p-2'>
                     Add Participant
                 </button>
             </div>
+            <AddParticipantModal modal={modal} setModal={setModal} />
         </>
     )
 }
---
+++ Frontend/merchant-client/src/Components/Popup/AddParticipantModal.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Users } from 'lucide-react'
+import queryUser from '../../Async logic/queryUser'
+import Dropdown from './Dropdown'
+function AddParticipantModal({ modal, setModal }) {
+    const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
+    const [selectedParticipants, setSelectedParticipants] = useState([])
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+        if (e.target.value !== '') {
+            queryUser().then((res) => {
+                setResults(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    }
+    return (
+        <>
+            <div id="add-modal" tabIndex="-1" className={`fixed ${modal == true ? `block` : `hidden`} z-50 inset-0 bg-opacity-60 overflow-y-auto h-full w-full px-4`}>
+                <div className="relative p-4 w-[90%] max-h-full m-[50px_auto]">
+
+                    <div className="relative bg-[#282828] w-[100%] rounded-lg shadow">
+                        <h1 className='text-slate-200'>Add Participants</h1>
+                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => { setModal(false) }}>
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+
+                        </div>
+                        <div>
+                            <input type='text' className='bg-gray-600 text-gray-100 w-[90%] m-4 rounded-md p-4 border-solid border-2 border-gray-200' value={search} onChange={handleChange} placeholder='Search Participants...'></input>
+                            <Dropdown names={results} search={search} selectedParticipants={selectedParticipants} setSelectedParticipants={setSelectedParticipants} />
+                        </div>
+
+                        <div className='flex m-4'>
+                            <Users color="#f4f1f1" />
+                            <h2 className='text-gray-100 font-bold ml-4'>Selected Participants</h2>
+                        </div>
+
+                        <div className='p-5'>
+                            <p className='text-gray-100'>Are you sure you want to add more participants?</p>
+                            <button type="button" className="mt-4 text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Add
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AddParticipantModal
