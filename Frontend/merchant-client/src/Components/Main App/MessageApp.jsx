80fd5b1488fe84c17ddafa48ad41a5f7788dc2aa
Shubhanker01 <shubhanker40@gmail.com>
2025-05-03 15:39:12 +0530

    shifted group to the topmost level

--- Frontend/merchant-client/src/Components/Main App/MessageApp.jsx
+++ Frontend/merchant-client/src/Components/Main App/MessageApp.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarApp from '../Header/NavbarApp'
 import CreateChat from './CreateChat'
 import ShowGroups from './ShowGroups'
 
 function MessageApp() {
-
+    const [groups, showGroups] = useState([])
     return (
         <>
             <NavbarApp />
             <div className="w-full fixed mx-auto my-6 p-4 bg-gray-800 h-full">
-                <ShowGroups />
+                <ShowGroups groups={groups} showGroups={showGroups} />
                 <CreateChat />
             </div>
 
--- Frontend/merchant-client/src/Components/Main App/ShowGroups.jsx
+++ Frontend/merchant-client/src/Components/Main App/ShowGroups.jsx
@@ -4,11 +4,12 @@ import { showGroupChat } from '../../Async logic/createChatGroup'
 import { useParams } from 'react-router-dom'
 import AboutGroup from '../Popup/AboutGroup'
 
-function ShowGroups() {
+function ShowGroups({ groups, showGroups }) {
   const params = useParams()
-  const [groups, showGroups] = useState([])
+  // const [groups, showGroups] = useState([])
   const [modal, setModal] = useState(false)
   const [participants, showParticipants] = useState([])
+
   useEffect(() => {
     showGroupChat(params.userId).then((res) => {
       showGroups(res)
@@ -19,11 +20,13 @@ function ShowGroups() {
       console.log(err)
     })
   }, [])
+
   if (groups.length == 0) {
     return <div>No groups to show</div>
   }
   return (
     <>
+      {console.log(groups)}
       <div className='mt-[50px] fixed z-20'>
         <h1 className='text-slate-200'>Your Groups</h1>
         {
@@ -38,7 +41,7 @@ function ShowGroups() {
                 <h1 className='text-slate-100 text-bold m-2'>{group.name}</h1>
                 <p className='text-slate-200'>{group.members.length} participants</p>
               </div>
-              <AboutGroup groupId={group._id} modal={modal} setModal={setModal} participants={group.members} admin={group.admin}/>
+              <AboutGroup groupId={group._id} modal={modal} setModal={setModal} participants={group.members} admin={group.admin} />
             </div>
 
 
