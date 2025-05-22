522e56d8c10ce7899f25d35a2a7a690c307415bd
Shubhanker01 <shubhanker40@gmail.com>
2025-05-22 13:50:15 +0530

    Displayed name and icon

--- Frontend/merchant-client/src/Components/Main App/Message.jsx
+++ Frontend/merchant-client/src/Components/Main App/Message.jsx
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid'
 import { socket } from '../../socket'
 import decodeToken from '../../utils/decodeJwt'
 import getCookie from '../../utils/getCookie'
+import { User } from 'lucide-react'
 
 function Message({ currentGroupChat }) {
     let token = getCookie()
@@ -49,11 +50,12 @@ function Message({ currentGroupChat }) {
     return (
         <>
             <div className="my-6 p-4 bg-gray-800 h-[85%] w-[100%] mr-4">
-                <div className="h-[90%] bg-gray-800 p-4 rounded shadow mb-4 overflow-y-auto">
+                <div className="fixed h-[70%] bg-gray-800 p-4 rounded shadow mb-4 overflow-y-auto w-[80%]">
                     {messages.map((msg) => (
-                        <div key={msg.id} className="p-2 border-b">
-                            <div>
-                                
+                        <div key={msg.id} className="p-2 bg-gray-700 border-b">
+                            <div className='flex'>
+                                <User color="#e7dada" />
+                                <p className='text-gray-200 ml-2'>{msg.user}</p>
                             </div>
                             <p className="text-gray-100">{msg.text}</p>
                             <span className="text-xs text-gray-100">
@@ -64,7 +66,7 @@ function Message({ currentGroupChat }) {
                     <div ref={messageEndRef}></div>
                 </div>
 
-                <div className="fixed sm:w-[45%] w-[70%] sm:bottom-[50px] bottom-[20px] flex gap-2">
+                {/* <div className="fixed sm:w-[45%] w-[70%] sm:bottom-[20px] bottom-[10px] flex gap-2">
                     <input
                         type="text"
                         className="flex-1 text-white p-2 border bg-slate-700 rounded"
@@ -78,9 +80,23 @@ function Message({ currentGroupChat }) {
                     >
                         Send
                     </button>
-                </div>
+                </div> */}
             </div>
-
+            <div className="fixed sm:w-[45%] w-[70%] sm:bottom-[20px] bottom-[10px] flex gap-2">
+                <input
+                    type="text"
+                    className="flex-1 text-white p-2 border bg-slate-700 rounded"
+                    placeholder="Type a message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded disabled"
+                    onClick={sendMsg} disabled={message === '' ? true : false}
+                >
+                    Send
+                </button>
+            </div>
         </>
     )
 }
