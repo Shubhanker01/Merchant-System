ed6b3bc8e1db1b0799b5fae16a7266ddeb601800
Shubhanker01 <shubhanker40@gmail.com>
2025-06-27 16:42:11 +0530

    messaging groups added

--- Frontend/merchant-client/src/Components/Main App/MessageApp.jsx
+++ Frontend/merchant-client/src/Components/Main App/MessageApp.jsx
@@ -10,7 +10,9 @@ function MessageApp() {
     const [groups, showGroups] = useState([])
     const [chatAdded, isChatAdded] = useState(false)
     const [currentGroupChat, showCurrentGroupChat] = useState('')
+    // variable to store and show the current group chats that user clicks in
     const [groupMessages, setGroupMessages] = useState({ group: "", messages: [] })
+    // receive unread messages incase of other groups
     const [otherMessages, setOtherMessages] = useState([])
 
     // connect the user socket instance with the namespace user
@@ -23,13 +25,18 @@ function MessageApp() {
         }
     }, [])
     useEffect(() => {
+        // receive the event from the server
         function receiveMessageEvent(arg) {
+            // if the message is received from other rooms
             if (arg.room !== currentGroupChat) {
                 console.log("This message is from other group chat")
+                // check if the room that message is received in is present in otherMessages variable
                 let index = otherMessages.find((group) => group.groupName == arg.room)
+                // if the message received from room is for the first time
                 if (index == undefined) {
                     setOtherMessages([...otherMessages, { groupName: arg.room, noOfMessages: 1 }])
                 }
+                // if the message was received before just increase the noOfMessages variable in the object
                 else {
                     setOtherMessages(otherMessages.map((group) => {
                         if (group.groupName == arg.room) {
@@ -39,6 +46,7 @@ function MessageApp() {
                     }))
                 }
             }
+            // just append the message in the current group
             else {
                 setGroupMessages({ ...groupMessages, group: currentGroupChat, messages: [...groupMessages.messages, arg.message] })
             }
@@ -51,22 +59,25 @@ function MessageApp() {
 
     }, [groupMessages, otherMessages])
     useEffect(() => {
+        // retrieve messages from database in case the user moves to other groups
         async function retrieveMessages() {
             if (currentGroupChat !== '') {
                 let data = await getOldMessages(currentGroupChat)
+                let arr = []
                 data.map((chat) => {
                     let msg = {
                         id: chat._id,
                         user: chat.username,
                         text: chat.message,
-                        timeStamp: chat.updatedAt
+                        timestamp: chat.updatedAt
                     }
-                    setGroupMessages({ ...setGroupMessages, group: currentGroupChat, messages: [...groupMessages.messages, msg] })
+                    arr.push(msg)
                 })
+                setGroupMessages({ ...groupMessages, group: currentGroupChat, messages: [...arr] })
             }
         }
         retrieveMessages()
-        // setGroupMessages({ ...setGroupMessages, group: currentGroupChat, messages: [] })
+        // if the user shifts to the group that has unread messages then just find the currentgroup chat and remove it from the otherMessages array
         let ind = otherMessages.findIndex((group) => group.groupName == currentGroupChat)
         if (ind !== -1) {
             setOtherMessages(otherMessages.filter((group) => {
@@ -76,10 +87,10 @@ function MessageApp() {
             }))
         }
     }, [currentGroupChat])
-    console.log(otherMessages)
+    // console.log(otherMessages)
+    // console.log(groupMessages)
     return (
         <>
-            {/* {console.log(tempMessages)} */}
             <NavbarApp />
             <div className="grid grid-cols-4 w-full mx-auto p-4 bg-gray-800 h-screen fixed">
                 <div>
