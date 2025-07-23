ef424882e154cff0963ca8d9ad6326aed8b7b264
Shubhanker01 <shubhanker40@gmail.com>
2025-07-23 17:36:20 +0530

    Performed Load tests

--- Backend/routes/merchant login/merchantLogin.js
+++ Backend/routes/merchant login/merchantLogin.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { limiter } = require('../../middlewares/rateLimiter')
+// const { limiter } = require('../../middlewares/rateLimiter')
 
 const merchantLogin = require('../../controllers/merchant login controller/merchantlogin.controller')
 
 
-router.route('/login').post(limiter, merchantLogin)
+// router.route('/login').post(limiter, merchantLogin)
+router.route('/login', merchantLogin)
 module.exports = router
