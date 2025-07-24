605be1350f2df864856fde6e288f54a9116015ce
Shubhanker01 <shubhanker40@gmail.com>
2025-07-24 17:26:50 +0530

    Working on search bar user bids

--- Backend/routes/merchant login/merchantLogin.js
+++ Backend/routes/merchant login/merchantLogin.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-// const { limiter } = require('../../middlewares/rateLimiter')
+const { limiter } = require('../../middlewares/rateLimiter')
 
 const merchantLogin = require('../../controllers/merchant login controller/merchantlogin.controller')
 
 
-// router.route('/login').post(limiter, merchantLogin)
-router.route('/login', merchantLogin)
+router.route('/login').post(limiter, merchantLogin)
+// router.route('/login').post(merchantLogin)
 module.exports = router
--- Frontend/merchant-client/src/Components/Body/UserBids.jsx
+++ Frontend/merchant-client/src/Components/Body/UserBids.jsx
@@ -3,13 +3,16 @@ import { Pencil, Trash } from 'lucide-react'
 import DeleteBid from '../Popup/DeleteBid'
 import UpdateBidForm from '../Popup/UpdateBidForm'
 import convertToDateFormat from '../../utils/convertToDateFormat'
+import SearchBar from './SearchBar'
+import useDebounce from '../../hooks/useDebounce'
 
 function UserBids({ bids, setBids }) {
     const [modal, setModal] = useState(false)
     const [updateModal, setUpdateModal] = useState(false)
     const [id, setId] = useState("")
+    const [searchBids, setSearchBids] = useState("")
     const [updateBidFields, setUpdateBidFields] = useState({ title: "", price: 0, closingDate: "" })
-
+    const debouncedValue = useDebounce(searchBids, 2000)
     const openDeleteBidModal = (id) => {
         setModal(true)
         setId(id)
@@ -22,6 +25,7 @@ function UserBids({ bids, setBids }) {
     if (bids.length == 0) {
         return <div>You have added no bids</div>
     }
+    console.log(debouncedValue)
     return (
         <>
             <div class="container mx-auto px-4 sm:px-8">
@@ -36,19 +40,9 @@ function UserBids({ bids, setBids }) {
                                     </svg>
                                 </div>
                             </div>
+                            <SearchBar searchBids={searchBids} setSearchBids={setSearchBids} />
+                        </div>
 
-                        </div>
-                        <div class="block relative">
-                            <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                                <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
-                                    <path
-                                        d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
-                                    </path>
-                                </svg>
-                            </span>
-                            <input placeholder="Search"
-                                class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
-                        </div>
                     </div>
                     <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                         <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
